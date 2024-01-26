b90987407a9920c44f64f775d4f992fc61c2425a
Meisterboom <lol@lol.de>
2024-01-26 10:39:47 +0100

    feat: Script: Added status script

---
+++ palworld-status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "-------------------------"
+# Set the service name
+SERVICENAME="palworld.service"
+
+# Check Status
+STATUS="$(systemctl is-active palworld.service)"
+
+if [ "${STATUS}" = "active" ]; then
+    echo "Server is running..."
+    echo "Have Fun!"
+    echo "-------------------------"
+    exit 1
+fi
+
+# If not active, retrieve the last complete status, including errors
+status_result=$(systemctl status $SERVICENAME 2>&1)
+
+echo "Server is offline!"
+echo "-------------------------"
+echo "$status_result"
